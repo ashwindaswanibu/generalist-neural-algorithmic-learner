e689b5a37223c6813c20bb93cceb3e0e307a0166
Ashwin <41562196+ashwindaswani@users.noreply.github.com>
2022-12-02 15:16:08 -0500

    Refactoring

--- train/train_multitask.py
+++ train/train_multitask.py
@@ -199,6 +199,119 @@ def collect_and_eval(sampler, predict_fn, sample_count, rng_key, spec, extras):
   return evaluate_preds(preds, outputs, hints, lengths, hint_preds, spec,
                         extras)
 
+def process_pred_as_input(spec, sample_iterator):
+  
+  def _iterate():
+    while True:
+      feedback = next(sample_iterator)
+      features = feedback.features
+      pred_h = [h for h in features.hints if h.name == 'pred_h']
+      if pred_h:
+        assert len(pred_h) == 1
+        pred_h = pred_h[0]
+        hints = [h for h in features.hints if h.name != 'pred_h']
+        for i in range(len(features.lengths)):
+          assert np.sum(np.abs(pred_h.data[1:int(features.lengths[i]), i] -
+                               pred_h.data[0, i])) == 0.0
+        inputs = tuple(features.inputs) + (
+            probing.DataPoint(name='pred', location=pred_h.location,
+                              type_=pred_h.type_, data=pred_h.data[0]),)
+        features = features._replace(inputs=tuple(inputs),
+                                     hints=tuple(hints))
+        feedback = feedback._replace(features=features)
+      yield feedback
+
+  new_spec = {}
+  for k in spec:
+    if k == 'pred_h':
+      assert spec[k] == (specs.Stage.HINT, specs.Location.NODE,
+                         specs.Type.POINTER)
+      new_spec['pred'] = (specs.Stage.INPUT, specs.Location.NODE,
+                          specs.Type.POINTER)
+    else:
+      new_spec[k] = spec[k]
+
+  return new_spec, _iterate()
+
+def process_random_pos(sample, rng):
+  def _iterate():
+    while True:
+      feedback = next(sample)
+      inputs = feedback.features.inputs
+      pos, = [x for x in inputs if x.name == 'pos']
+      batch_size, num_nodes = pos.data.shape
+      unsorted = rng.uniform(size=(batch_size, num_nodes))
+      new_pos = []
+      for i in range(batch_size):  
+        split, = np.where(pos.data[i] == 0)
+        split = np.concatenate([split, [num_nodes]])
+        new_pos.append(
+            np.concatenate([np.sort(unsorted[i, split[j]:split[j+1]]) for j in range(len(split) - 1)]))
+      pos.data = np.array(new_pos)
+      inputs = [(pos if x.name == 'pos' else x) for x in inputs]
+      features = feedback.features._replace(inputs=inputs)
+      feedback = feedback._replace(features=features)
+      yield feedback
+
+  return _iterate()
+
+def _preprocess_permutations(probes, enforce_permutations):
+ 
+  output = []
+  for x in probes:
+    if x.type_ != specs.Type.SHOULD_BE_PERMUTATION:
+      output.append(x)
+      continue
+    assert x.location == specs.Location.NODE
+    if enforce_permutations:
+      new_x, mask = probing.predecessor_to_cyclic_predecessor_and_first(x.data)
+      output.append(
+          probing.DataPoint(
+              name=x.name,
+              location=x.location,
+              type_=specs.Type.PERMUTATION_POINTER,
+              data=new_x))
+      output.append(
+          probing.DataPoint(
+              name=x.name + '_mask',
+              location=x.location,
+              type_=specs.Type.MASK_ONE,
+              data=mask))
+    else:
+      output.append(probing.DataPoint(name=x.name, location=x.location,
+                                      type_=specs.Type.POINTER, data=x.data))
+  return output
+
+
+def process_permutations(spec, sample_iterator, enforce_permutations):
+  def _iterate():
+    while True:
+      feedback = next(sample_iterator)
+      features = feedback.features
+      inputs = _preprocess_permutations(features.inputs, enforce_permutations)
+      hints = _preprocess_permutations(features.hints, enforce_permutations)
+      outputs = _preprocess_permutations(feedback.outputs, enforce_permutations)
+      features = features._replace(inputs=tuple(inputs),
+                                   hints=tuple(hints))
+      feedback = feedback._replace(features=features,
+                                   outputs=outputs)
+      yield feedback
+
+  new_spec = {}
+  for k in spec:
+    if (spec[k][1] == specs.Location.NODE and
+        spec[k][2] == specs.Type.SHOULD_BE_PERMUTATION):
+      if enforce_permutations:
+        new_spec[k] = (spec[k][0], spec[k][1], specs.Type.PERMUTATION_POINTER)
+        new_spec[k + '_mask'] = (spec[k][0], spec[k][1], specs.Type.MASK_ONE)
+      else:
+        new_spec[k] = (spec[k][0], spec[k][1], specs.Type.POINTER)
+    else:
+      new_spec[k] = spec[k]
+
+  return new_spec, _iterate()
+
+
 def evaluate(rng_key, model, feedback, spec, extras=None, verbose=False):
   """Evaluates a model on feedback."""
   out = {}
@@ -235,133 +348,3 @@ def _concat(dps, axis):
   return jax.tree_util.tree_map(lambda *x: jnp.concatenate(x, axis), *dps)   
 
 
-def process_pred_as_input(spec, sample_iterator):
-  """Move pred_h hint to pred input."""
-  def _iterate():
-    while True:
-      feedback = next(sample_iterator)
-      features = feedback.features
-      pred_h = [h for h in features.hints if h.name == 'pred_h']
-      if pred_h:
-        assert len(pred_h) == 1
-        pred_h = pred_h[0]
-        hints = [h for h in features.hints if h.name != 'pred_h']
-        for i in range(len(features.lengths)):
-          assert np.sum(np.abs(pred_h.data[1:int(features.lengths[i]), i] -
-                               pred_h.data[0, i])) == 0.0
-        inputs = tuple(features.inputs) + (
-            probing.DataPoint(name='pred', location=pred_h.location,
-                              type_=pred_h.type_, data=pred_h.data[0]),)
-        features = features._replace(inputs=tuple(inputs),
-                                     hints=tuple(hints))
-        feedback = feedback._replace(features=features)
-      yield feedback
-
-  new_spec = {}
-  for k in spec:
-    if k == 'pred_h':
-      assert spec[k] == (specs.Stage.HINT, specs.Location.NODE,
-                         specs.Type.POINTER)
-      new_spec['pred'] = (specs.Stage.INPUT, specs.Location.NODE,
-                          specs.Type.POINTER)
-    else:
-      new_spec[k] = spec[k]
-
-  return new_spec, _iterate()
-
-def process_random_pos(sample_iterator, rng):
-  """Randomize the `pos` input from a sampler.
-  The `pos` input is, by default, a scalar uniformly spaced between 0 and 1
-  across the nodes. The exception are string algorithms (naive_string_matcher,
-  kmp_string_matcher and lcs_length), where the `pos` sequence is split into
-  needle and haystack (or first and second string, for lcs_length). To avoid 
-  overfitting to these linearly spaced values during training, we replaced them 
-  with random values, uniformly sampled in [0, 1].
-  Parameters:
-    sample_iterator: An iterator producing samples with non-random `pos` inputs.
-    rng: Numpy random generator
-  Returns:
-    An iterator returning the samples with randomized `pos` inputs.
-  """
-  def _iterate():
-    while True:
-      feedback = next(sample_iterator)
-      inputs = feedback.features.inputs
-      pos, = [x for x in inputs if x.name == 'pos']
-      batch_size, num_nodes = pos.data.shape
-      unsorted = rng.uniform(size=(batch_size, num_nodes))
-      new_pos = []
-      for i in range(batch_size):  # we check one example at a time.
-        # We find if there are splits in the pos sequence, marked by zeros.
-        # We know there will always be at least 1 zero, if there's no split.
-        split, = np.where(pos.data[i] == 0)
-        split = np.concatenate([split, [num_nodes]])
-        # We construct the randomized pos by sorting the random values in each
-        # split and concatenating them.
-        new_pos.append(
-            np.concatenate([np.sort(unsorted[i, split[j]:split[j+1]])
-                            for j in range(len(split) - 1)]))
-      pos.data = np.array(new_pos)
-      inputs = [(pos if x.name == 'pos' else x) for x in inputs]
-      features = feedback.features._replace(inputs=inputs)
-      feedback = feedback._replace(features=features)
-      yield feedback
-
-  return _iterate()
-
-def _preprocess_permutations(probes, enforce_permutations):
-  """Replace should-be permutations with proper permutation pointer + mask."""
-  output = []
-  for x in probes:
-    if x.type_ != specs.Type.SHOULD_BE_PERMUTATION:
-      output.append(x)
-      continue
-    assert x.location == specs.Location.NODE
-    if enforce_permutations:
-      new_x, mask = probing.predecessor_to_cyclic_predecessor_and_first(x.data)
-      output.append(
-          probing.DataPoint(
-              name=x.name,
-              location=x.location,
-              type_=specs.Type.PERMUTATION_POINTER,
-              data=new_x))
-      output.append(
-          probing.DataPoint(
-              name=x.name + '_mask',
-              location=x.location,
-              type_=specs.Type.MASK_ONE,
-              data=mask))
-    else:
-      output.append(probing.DataPoint(name=x.name, location=x.location,
-                                      type_=specs.Type.POINTER, data=x.data))
-  return output
-
-
-def process_permutations(spec, sample_iterator, enforce_permutations):
-  """Replace should-be permutations with proper permutation pointer + mask."""
-  def _iterate():
-    while True:
-      feedback = next(sample_iterator)
-      features = feedback.features
-      inputs = _preprocess_permutations(features.inputs, enforce_permutations)
-      hints = _preprocess_permutations(features.hints, enforce_permutations)
-      outputs = _preprocess_permutations(feedback.outputs, enforce_permutations)
-      features = features._replace(inputs=tuple(inputs),
-                                   hints=tuple(hints))
-      feedback = feedback._replace(features=features,
-                                   outputs=outputs)
-      yield feedback
-
-  new_spec = {}
-  for k in spec:
-    if (spec[k][1] == specs.Location.NODE and
-        spec[k][2] == specs.Type.SHOULD_BE_PERMUTATION):
-      if enforce_permutations:
-        new_spec[k] = (spec[k][0], spec[k][1], specs.Type.PERMUTATION_POINTER)
-        new_spec[k + '_mask'] = (spec[k][0], spec[k][1], specs.Type.MASK_ONE)
-      else:
-        new_spec[k] = (spec[k][0], spec[k][1], specs.Type.POINTER)
-    else:
-      new_spec[k] = spec[k]
-
-  return new_spec, _iterate()
